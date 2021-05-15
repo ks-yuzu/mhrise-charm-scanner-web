0a9e2dcc4f96f89dc5682fc8ee26aad3121442d3
ks-yuzu <yuzu6120@gmail.com>
2021-05-16 03:39:47 +0900

    Update to fetch assets in parallel

--- src/util.js
+++ src/util.js
@@ -59,3 +59,34 @@ export function getMostMatchedImage(image, templates, point, maskBinaryThreshold
 
   return candidate;
 }
+
+
+
+export function promiseAllRecursive(value) {
+  if (value instanceof Promise) {
+    return value
+  }
+
+  if (Array.isArray(value)) {
+    return Promise.all(value.map(promiseAllRecursive))
+  }
+
+  if (typeof value === 'object') {
+    return resolveObject(value)
+  }
+
+  return Promise.resolve(value)
+}
+
+function resolveObject(object) {
+  const promises = Object.keys(object).map(key => {
+    return promiseAllRecursive(object[key]).then(value => ({ key, value }))
+  });
+
+  return Promise.all(promises).then(results => {
+    return results.reduce((obj, pair) => {
+      obj[pair.key] = pair.value
+      return obj
+    }, {})
+  })
+}
