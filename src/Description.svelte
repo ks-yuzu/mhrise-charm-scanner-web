0347cd0b0980460391b017c79b5015e6b7e75f7a
ks-yuzu <yuzu6120@gmail.com>
2021-05-16 03:40:35 +0900

    Update description

--- src/Description.svelte
+++ src/Description.svelte
@@ -1,15 +1,30 @@
 <div id="description" class="card border border-light shadow-sm">
   <div class="card-body">
-  <p style="margin: auto; max-width: 100%; width: 54rem; text-align: left">
-    モンスターハンターライズの護石を自動読み取りするツールです。<br>
-    Nintendo Switch の 30 秒キャプチャ動画を用意するだけで, 護石のスキルやスロットが読み取れます。
-    (<a href="sample/input.mp4">動画例</a>)<br>
-    <br>
-    出力形式は、&lt;スキル1&gt;,&lt;スキル1Lv&gt;,&lt;スキル2&gt;,&lt;スキル2Lv&gt;,&lt;スロット1Lv&gt;,&lt;スロット2Lv&gt;,&lt;スロット3Lv&gt; です。<br>
-    <a href="https://mhrise.wiki-db.com/sim/">泣きシミュ</a> さんでそのままインポートできます。<br>
-    <br>
-    機能追加、UI 改善は随時行っていきます。
-  </p>
+    <!-- <p style="margin: auto; max-width: 100%; width: 54rem; text-align: left"> -->
+    <p>
+      モンスターハンターライズの護石用ツールです。<br>
+      機能追加、UI 改善は随時行っています。<br>
+    </p>
+
+    <h3>■ 護石スキャン</h3>
+    <p>
+      護石のスキルやスロットを自動読み取りします。<br>
+      Nintendo Switch の 30 秒キャプチャ動画を用意するだけで、自動でデータ化できます。
+      (<a href="sample/input.mp4">動画例</a>)<br>
+      <br>
+      出力形式は、&lt;スキル1&gt;,&lt;スキル1Lv&gt;,&lt;スキル2&gt;,&lt;スキル2Lv&gt;,&lt;スロット1Lv&gt;,&lt;スロット2Lv&gt;,&lt;スロット3Lv&gt; です。<br>
+      <a href="https://mhrise.wiki-db.com/sim/">泣きシミュ</a> さんでそのままインポートできます。<br>
+    </p>
+
+    <h3>■ 護石管理</h3>
+    <p>
+      スキャンした護石の一覧を見ることができます。<br>
+      護石データは蓄積され、次回、同じブラウザで開いた時にも保持されます。<br>
+      <br>
+      表中の "合計スキルレベル" は、<br>
+      <span class="px-3 font-weight-bold">(スキルを装飾品換算した場合のスロットLv) × (スキルLv) + (スロットLv)</span><br>
+      として計算しています。<br>
+    </p>
   </div>
 </div>
 
@@ -20,5 +35,15 @@
 
     max-width: 100%;
     width: 56rem;
+  }
+
+  #description h3 {
+    font-size:   1rem;
+    font-weight: bold;
+    text-align:  left;
+  }
+
+  #description p {
+    text-align: left;
   }
 </style>
