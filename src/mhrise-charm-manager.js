9d77bc5d219c74e5ac17cc099e1620ddee47fada
ks-yuzu <yuzu6120@gmail.com>
2021-05-18 13:38:07 +0900

    Implement substitutable charm search fuction

--- src/mhrise-charm-manager.js
+++ src/mhrise-charm-manager.js
@@ -1,7 +1,11 @@
+import {skillToSlotLevel} from './mhrise-charm-decorations.js'
+
 export default class MHRiseCharmManager {
+  MAX_SKILLS = 2
+  MAX_SLOTS  = 3
+
   db        = null              // WebSQL
   indexeddb = null              // IndexedDB
-
 
   constructor() {
     this.db = openDatabase('mhrise-charm-manager', '', 'MHRise charm manager', 5000)
@@ -59,6 +63,10 @@ export default class MHRiseCharmManager {
                slot3       int,
                imagename   varchar(128),
                unique (skill1, skill1Level, skill2, skill2Level, slot1, slot2, slot3))`)
+
+    await this.sql(`create index if not exists 'charms.skill1' on charms(skill1, skill1Level)`)
+    await this.sql(`create index if not exists 'charms.skill2' on charms(skill2, skill2Level)`)
+    await this.sql(`create index if not exists 'charms.slots' on charms(slot1, slot2, slot3)`)
   }
 
 
@@ -67,5 +75,77 @@ export default class MHRiseCharmManager {
     const img = cv.matFromArray(result.rows, result.cols, result.type, result.data)
 
     return img
+  }
+
+
+  async findSubstitutableCharms(charm) {
+    // interface Charm {
+    //   skills:      string[]
+    //   skillLevels: number[]
+    //   slots:       number[]
+    // }
+    const sl = []
+    const substitutableCharms = []
+
+    for (sl[0] = charm.skillLevels[0]; sl[0] >= 0; sl[0]--) {
+      for (sl[1] = charm.skillLevels[1]; sl[1] >= 0; sl[1]--) {
+        // console.log(sl[0], sl[1])
+        // 装飾品で実現するスキルレベル
+        const skillLevelsRequiredInDecorations = [...Array(this.MAX_SKILLS).keys()].map(i => charm.skillLevels[i] - sl[i])
+
+        // スキル用の要求スロット数
+        const slotRequirementForSkill = skillLevelsRequiredInDecorations.reduce(((a, b) => a + b), 0)
+
+        // 装飾品の要求スロット数 + charm のスロット数 > 3 なら実現不可能
+        if ( slotRequirementForSkill + charm.slots.filter(i => i).length > this.MAX_SLOTS ) {
+          // それ以上 sl (=スキル要求) を減らして探索しても意味がない
+          break
+        }
+
+        const requiredSlots = [
+          ...charm.slots,
+          ...Array.from( {length: skillLevelsRequiredInDecorations[0]}, () => skillToSlotLevel[charm.skills[0]] ),
+          ...Array.from( {length: skillLevelsRequiredInDecorations[1]}, () => skillToSlotLevel[charm.skills[1]] ),
+        ].filter(i => i).sort().reverse()
+
+        // conbination にしないといけない
+        const skill1Constraints = sl[0] ? `(skill1 = '${charm.skills[0]}' and skill1Level >= ${sl[0]})` : ''
+        const skill2Constraints = sl[1] ? `(skill2 = '${charm.skills[1]}' and skill2Level >= ${sl[1]})` : ''
+        const skillConstraints = [skill1Constraints, skill2Constraints].filter(i => i).join(' and ')
+
+        const skill1ConstraintsRev = sl[1] ? `(skill1 = '${charm.skills[1]}' and skill1Level >= ${sl[1]})` : ''
+        const skill2ConstraintsRev = sl[0] ? `(skill2 = '${charm.skills[0]}' and skill2Level >= ${sl[0]})` : ''
+        const skillConstraintsRev  = [skill1ConstraintsRev, skill2ConstraintsRev].filter(i => i).join(' and ')
+
+        const sc = skillConstraints.length
+              ? '(' + [`(${skillConstraints})`, `(${skillConstraintsRev})`].filter(i => i).join(' or ') + ') and '
+              : ''
+
+        const query =`select rowid,* from charms where ${sc} slot1 >= ${requiredSlots[0] || 0} and slot2 >= ${requiredSlots[1] || 0} and slot3 >= ${requiredSlots[2] || 0}`
+        // console.log(query)
+        const {result} = await this.sql(query)
+        // console.log(result.rows)
+
+        substitutableCharms.push(
+          ...[...result.rows].filter(i => !this._isSameCharm(this._row2obj(i), charm))
+        )
+        // TODO: 合計スキルレベルの条件 (WebSQL 用) は必要？
+      }
+    }
+
+    return substitutableCharms
+  }
+
+  // TODO: charm class 作ってコンストラクタでやる
+  _row2obj(row) {
+    return {
+      skills:      [row.skill1, row.skill2],
+      skillLevels: [row.skill1Level, row.skill2Level],
+      slots:       [row.slot1, row.slot2, row.slot3],
+    }
+  }
+
+  _isSameCharm(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b)
   }
 }
