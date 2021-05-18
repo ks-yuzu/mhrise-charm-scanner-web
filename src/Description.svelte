9b8f44ffc1f481936648ef62a66145f22ff43d23
ks-yuzu <yuzu6120@gmail.com>
2021-05-18 13:39:16 +0900

    Update description

--- src/Description.svelte
+++ src/Description.svelte
@@ -1,10 +1,15 @@
 <div id="description" class="card border border-light shadow-sm">
   <div class="card-body">
-    <!-- <p style="margin: auto; max-width: 100%; width: 54rem; text-align: left"> -->
+    <h3>■ 概要</h3>
     <p>
       モンスターハンターライズの護石用ツールです。<br>
       機能追加、UI 改善は随時行っています。<br>
     </p>
+  </div>
+</div>
+
+<div id="description" class="card border border-light shadow-sm">
+  <div class="card-body">
 
     <h3>■ 護石スキャン</h3>
     <p>
@@ -14,22 +19,30 @@
       <br>
       出力形式は、&lt;スキル1&gt;,&lt;スキル1Lv&gt;,&lt;スキル2&gt;,&lt;スキル2Lv&gt;,&lt;スロット1Lv&gt;,&lt;スロット2Lv&gt;,&lt;スロット3Lv&gt; です。<br>
       <a href="https://mhrise.wiki-db.com/sim/">泣きシミュ</a> さんでそのままインポートできます。<br>
+      <br>
+      (注意)<br>
+      - <span style="color: red">装飾品を外した状態でないと、装飾品分のスキルも読み込まれてしまいます。</span> (一括解除が有用です)<br>
+      - キャプチャ動画のファイル名はデフォルトのまま使用することを推奨します。<br>
     </p>
+  </div>
+</div>
+<div id="description" class="card border border-light shadow-sm">
+  <div class="card-body">
 
     <h3>■ 護石管理</h3>
     <p>
       スキャンした護石の一覧を見ることができます。<br>
-      右端の画像ボタンを押すことで、スキャン時のスクリーンショットを確認できます。<br>
       護石データは蓄積され、次回、同じブラウザで開いた時にも保持されます。<br>
       <br>
       表中の "合計スキルレベル" は、<br>
       <span class="px-3 font-weight-bold">(スキルを装飾品換算した場合のスロットLv) × (スキルLv) + (スロットLv)</span><br>
       として計算しています。<br>
       <br>
-      例) 納刀術2, ひるみ軽減1, スロット 3-1-0 の場合<br>
+      例: 納刀術2, ひるみ軽減1, スロット 3-1-0 の場合<br>
       <span class="px-3">2 × 2 + 1 × 1 + 3 + 1 + 0 = 9</span><br>
-
-      近日、上位互換護石の表示機能を追加予定です。
+      <br>
+      右端の画像ボタンを押すことで、スキャン時のスクリーンショットを確認できます。<br>
+      右端に上向き矢印がある場合、上位互換の護石があります。クリックで該当の護石を確認することができます。<br>
     </p>
   </div>
 </div>
