0f0d9a427fd9c3569e8aafcaa91be0ecfb66cac1
ks-yuzu <yuzu6120@gmail.com>
2022-03-26 19:07:08 +0900

    Add sentry notification

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import App from './App.svelte';
+import * as Sentry from "@sentry/browser";
+import { BrowserTracing } from "@sentry/tracing";
 
+Sentry.init({
+  dsn: "https://ae96a41629e040f59f30ad89eb99d4f2@o1178530.ingest.sentry.io/6290379",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0, // 1.0 -> 100%
+})
+
+
+import App from './App.svelte';
 const app = new App({
 	target: document.body,
 	props: {
